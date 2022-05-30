c626a19ef803a3be710a02ace1e31b74446c7cf6
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2022-05-30 10:10:43 +0530

    CL:1.04:Fixed some bugs and added CSS

--- pnf/src/app/app-routing.module.ts
+++ pnf/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {path:'pnf',component:PNFComponent},
-  {path:'',redirectTo:'pnf',component:PNFComponent},
+  {path:'**',component:PNFComponent},
 ];
 
 @NgModule({
