dda4ec36c98a6efd40a0228ebff6d5b63423d8f9
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2022-05-30 09:17:39 +0530

    CL:1.03:Added  routing for pnf

--- pnf/src/app/app-routing.module.ts
+++ pnf/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
+import { PNFComponent } from './pnf/pnf.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'pnf',component:PNFComponent},
+  {path:'',redirectTo:'pnf',component:PNFComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
